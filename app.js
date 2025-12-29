51b3dbee35c3e840bdb4a65afa92c4ed3f23b5e7
Reem99-sina <reemsina2@gmail.com>
2025-12-29 15:26:34 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const serverless = require("serverless-http");
+const { connectdb } = require("./connect");
+const userRouter = require("./users/users.router");
+const postsRouter = require("./posts/posts.router");
+const likesRouter = require("./likes/likes.router");
+const commentsRouter = require("./comments/comment.router");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/like", likesRouter);
+app.use("/comment", commentsRouter);
+app.use("/post", postsRouter);
+
+
+
+connectdb();
+
+// const server = http.createServer(app);
+
+// const PORT = process.env.PORT || 1200;
+// server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+module.exports.handler = serverless(app);
