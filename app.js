d261773de8f5a4a951436ae0e5b4d92ad0251dc0
reem-ebrahim <reem@leaptechkw.com>
2026-03-24 14:09:11 +0200

    edit tiktok

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ const postsRouter = require("./posts/posts.router");
 const likesRouter = require("./likes/likes.router");
 const commentsRouter = require("./comments/comment.router");
 // require("./uploads/picture")
-const cors=require("cors")
+const cors=require("cors");
+const path = require("path");
 
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors())
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/uploads/picture", express.static("./uploads/picture"));
 app.use("/user", userRouter);
 app.use("/like", likesRouter);
